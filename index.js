6b0c4162dd74ab4b3cfdbf1b3061140c44233117
unknown <aisaac@MEDNET-1Y2TL12.mednetstudy.com>
2015-09-17 13:54:03 -0500

    Authentication and "routing"

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var bodyParser = require('body-parser');
 var tokenAuth = require('./token-auth');
 var httpProxy = require('http-proxy');
 var http = require('http');
+var url = require('url');
 
 var PORT  = process.env.PORT || 3000;
 
@@ -15,10 +16,9 @@ var app = connect();
 app.use(bodyParser.json());
 app.use(tokenAuth.authenticate);
 app.use(function(req, res) {
-    proxyServer.web(req, res, { target: 'https://aisaac01.imednet.com/api.php' });
+    req.url = url.format({'query': {'study': req.token.study, 'resource' : req.token.resource }, 'pathname': 'api.php'});
+    proxyServer.web(req, res, { target: 'https://aisaac01.imednet.com/' });
 });
 http.createServer(app).listen(PORT);
 
-console.log('Token Auth Server on: %j', PORT);
-
-
+console.log('auth-token-proxy listening on: %j', PORT);
--- token-auth.js
+++ token-auth.js
@@ -2,18 +2,9 @@
 
 var datastore = require('./datastore');
 
-module.exports.authenticate = function authenticate(req, res, next) {
-    if (validateAuthenticationToken(req, res, next)) {
-        next();
-    } else {
-        res.writeHead(403, {'Content-type' : 'application/json'});
-        res.end(JSON.stringify({'msg' :'Not Authorised'}));
-    }
-};
-
-function validateAuthenticationToken(req, res, next) {
+module.exports.authenticate = function validateAuthenticationToken(req, res, next) {
     if (req.headers['authorization']) {
-        var accessTokenPair = req.headers['authorization'].replace('Bearer ', '').split(':');
+        var accessTokenPair = req.headers['authorization'].replace('Bearer', '').trim().split(':');
         if (accessTokenPair && accessTokenPair.length === 2) {
             datastore.locate({apiId: accessTokenPair[0], apiSecret: accessTokenPair[1]}, function(err, token){
                 if (err || !token) {
@@ -28,7 +19,7 @@ function validateAuthenticationToken(req, res, next) {
             notAuthorized(res)
     } else
         notAuthorized(res)
-}
+};
 
 function notAuthorized(res) {
     res.writeHead(403, {'Content-type' : 'application/json'});
