59c84ab723346a4c78d13aeda03368a6523b1d83
Ash Isaac <aisaac@mednetstudy.com>
2015-09-21 11:03:57 -0500

    Added expires to proxied data

--- auth-proxy.js
+++ auth-proxy.js
@@ -18,13 +18,21 @@ module.exports.run = function(options) {
 
     var proxyServer = httpProxy.createProxyServer({ secure: false });
     var app = connect();
-
     app.use(bodyParser.json());
+    //middleware blacklist
     app.use(blacklist.block);
+    //middleware token-auth
     app.use(tokenAuth.authenticate);
     app.use(function(req, res) {
         req.url = url.format({
-            'query': {'study': req.token.study, 'resource' : req.token.resource, 'clientId': req.token.clientId, 'url':req.url, 'ip': req.ip },
+            'query': {
+                'study': req.token.study,
+                'resource': req.token.resource,
+                'clientId': req.token.clientId,
+                'url': req.url,
+                'ip': req.ip,
+                'expires': String(req.token.expires)
+            },
             'pathname': path
         });
         proxyServer.web(req, res, { target: target });
